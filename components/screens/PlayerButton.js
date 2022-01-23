05f08116676cd78366d6e92e0bd637754cc0251b
dmitkon <dmitriy.kononec@mail.ru>
2022-01-23 03:15:59 +0300

    player_button_demo

--- components/screens/PlayerButton.js
+++ components/screens/PlayerButton.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, SafeAreaView } from 'react-native';
+import SystemButton from '../items/SystemButton';
+import Display from '../items/Display';
+import { gStyle } from '../../styles/GeneralStyles';
+import { getLight } from '../../src/lampLights';
 
 const PlayerButton = ({route}) => {
+    const [systemStatus, setSystemStatus] = useState("Система не запущена");
+    const [lampLight, setLampLight] = useState(getLight(0));
+
+    const pushHandle = () => {
+        console.log("push");
+    };
+
     return(
-        <SafeAreaView>
-            <Text>Hello {route.params.playerName}!</Text>
+        <SafeAreaView style={gStyle.screen}>
+            <Text style={gStyle.brainTitle}>{route.params.playerName}</Text>
+            
+            <Display status={systemStatus} light={lampLight} />
+
+            <SystemButton nameIcon='thumbs-up' onPressIn={pushHandle} />
         </SafeAreaView>
     );
 };
